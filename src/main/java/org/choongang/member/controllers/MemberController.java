16327f9bebc340786e4bc2b13ad1689a22f2e5d1
soeunl <soeun318@gmail.com>
2024-06-26 11:23:07 +0900

    포켓몬 게시판 로그인 작업

--- src/main/java/org/choongang/member/controllers/MemberController.java
+++ src/main/java/org/choongang/member/controllers/MemberController.java
@@ -1,11 +1,13 @@
 package org.choongang.member.controllers;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.choongang.global.config.annotations.Controller;
 import org.choongang.global.config.annotations.GetMapping;
 import org.choongang.global.config.annotations.PostMapping;
 import org.choongang.global.config.annotations.RequestMapping;
 import org.choongang.member.services.JoinService;
+import org.choongang.member.services.LoginService;
 
 @Controller
 @RequestMapping("/member") // 항상 앞에 주소는 member로 매칭이 됩니다
@@ -13,6 +15,7 @@ import org.choongang.member.services.JoinService;
 public class MemberController {
     
     private final JoinService joinService; // 매번 객체를 만들 필요 없이 주입
+    private final LoginService loginService;
     
     // 회원가입 양식
     @GetMapping("/join") // /memebr/join으로 유입됩니다
@@ -22,9 +25,16 @@ public class MemberController {
     
     // 회원가입 처리
     @PostMapping("/join")
-    public String joinPs(RequestJoin form) { // 알아서 값이 다 주입된다
+    public String joinPs(RequestJoin form, HttpServletRequest request) { // 알아서 값이 다 주입된다
+
         joinService.process(form);
-        return "member/join";
+
+        String url = request.getContextPath() + "/member/login";
+        String script = String.format("parent.location.replace('%s');", url);
+
+        request.setAttribute("script", script);
+
+        return "commons/execute_script";
     }
     
     // 로그인 양식
@@ -33,10 +43,13 @@ public class MemberController {
         return "member/login";
     }
 
+
     // 로그인 처리
     @PostMapping("/login")
-    public String loginPs(RequestJoin form) { // 값이 알아서 주입된다
-        // 양식을 제출하면 데이터가 넘어온다
-        return null;
+    public String loginPs(RequestLogin form) {
+
+        loginService.process(form);
+
+        return "commons/execute_script";
     }
 }
